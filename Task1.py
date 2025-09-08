7c439685101276db7c259dbf6881e37e3eb51e68
“rohit-kumar181” <rohitdrbcrec@gmail.com>
2025-09-08 18:28:01 +0530

    Uploading Task1.py and Task2.py files.

---
+++ Task1.py
@@ -0,0 +1,9 @@
+a = input("Enter the first number:")
+b = input("Enter the second number:")
+a = int(a)
+b = int(b)
+
+print("\nAddition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+FirstName = input("Enter ypur first name:")
+LastName = input("Enter your last name:")
+
+print('Hello, ' + FirstName +' '+ LastName +'!' + ' Welcome to the Python program.')
